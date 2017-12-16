085fb1e0e419560670a72c30ee31d16a8e39ba29
PoisedBullet <18317686+PoisedBullet@users.noreply.github.com>
2017-12-15 19:10:28 -0600

    Add files via upload

---
+++ user.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Tell me a name for the user"
+echo "Please, use one word only, and no special characters"
+read -r USER #Gets the username from user input
+echo "What password should they have?"
+read -r PASSWORD #Gets the password from user input
+PASSENC="$(openssl passwd -1 $PASSWORD)" #Salts the password
+cd /etc/ipsec.d
+echo "$USER:$PASSENC:xauth-psk" >> passwd 
+cd /etc/ppp/
+echo "\"$USER\" l2tpd \"$PASSWORD\" *" >> chap-secrets
